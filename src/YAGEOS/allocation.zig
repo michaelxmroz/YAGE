e546a660018800c663acedb9cc7427c11ca424a8
michaelxmroz <95481575+michaelxmroz@users.noreply.github.com>
2025-03-18 11:24:18 +0100

    Extended allocator to provide zig FixedBufferAllocators, minor refactorings to the allocator

--- src/YAGEOS/allocation.zig
+++ src/YAGEOS/allocation.zig
@@ -1,7 +1,8 @@
-// Simple two-bucket allocator for emulator operation, plus a small linear buffer for other run-time requirements
+// Simple two-bucket allocator for emulator operation, plus a small linear buffer for run-time zig allocation
 
 const log = @import("log.zig");
 const utils = @import("utils.zig");
+const std = @import("std");
 
 const HEAP_START = 0x1000000; // 16 mb for the kernel + stack
 const BUCKET_SIZE = 0x2000000; // 32 mb for the emulator.
@@ -10,7 +11,7 @@ const SCRAP_BUFFER_START = HEAP_START + BUCKET_COUNT * BUCKET_SIZE;
 const SCRAP_BUFFER_SIZE = 0x1000000; // 16 mb for the scrapspace
 const SCRAP_BUFFER_END = SCRAP_BUFFER_START + SCRAP_BUFFER_SIZE;
 
-const ALLOC_LOGS = true;
+const ALLOC_LOGS = false;
 
 const Bucket = struct
 {
@@ -41,7 +42,7 @@ var allocatorData = AllocatorData
     .scrapNextFree = SCRAP_BUFFER_START,
 };
 
-pub fn activeBucketAlloc(size : u64) *u64
+pub fn activeBucketAlloc(size : u64) *u8
 {
     const bucket = &allocatorData.buckets[allocatorData.currentActiveBucket];
     const retAddr = bucket.nextFree;
@@ -66,4 +67,25 @@ pub fn activeBucketFree() void
 {
     const bucket = &allocatorData.buckets[allocatorData.currentActiveBucket];
     bucket.nextFree = bucket.start;
+}
+
+pub fn scrapMemoryAlloc(size : u64) [*]u8
+{
+    const retAddr = allocatorData.scrapNextFree;
+    
+    allocatorData.scrapNextFree += size;
+    if(allocatorData.scrapNextFree > SCRAP_BUFFER_END)
+    {
+        log.ERROR("Exceeded scrap space capacity by {} bytes\n", .{ allocatorData.scrapNextFree - SCRAP_BUFFER_END });
+        utils.hang();
+    }
+
+    return @ptrFromInt(retAddr);
+}
+
+pub fn getAllocator(size : u64) std.heap.FixedBufferAllocator
+{
+    const mem = scrapMemoryAlloc(size);
+    const slice  : []u8 = mem[0..size];
+    return std.heap.FixedBufferAllocator.init(slice);
 }
